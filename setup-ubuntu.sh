3dabc40cff4e3e4522f8822c11b5aa36b32dcfed
vavasing <vaibhavsinghacads@gmail.com>
2019-04-10 20:06:33 +0000

    added setup script for Ubuntu

---
+++ setup-ubuntu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#ADS Project deployment steps on Ubuntu
+
+apt update -y
+#git clone https://github.com/vaibhavsingh1993/Gossip-Protocol
+apt install -y default-jdk
+apt-get install -y wget
+apt-get install -y unzip
+apt-get install -y gradle
