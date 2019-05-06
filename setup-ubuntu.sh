8ca0d673b4774f96ad3f46352b7e07d6e2cf9460
Vaibhav Singh <vaibhavsinghacads@gmail.com>
2019-05-06 11:10:09 -0400

    Added ansible installation to setup script

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,6 +4,8 @@
 
 apt update -y
 #git clone https://github.com/vaibhavsingh1993/Gossip-Protocol
+apt-get install ansible -y
+pip install requests google-auth
 apt install -y default-jdk
 apt-get install -y wget
 apt-get install -y unzip
