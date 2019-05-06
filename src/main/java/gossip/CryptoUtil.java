e3a92513117bf0e52487acbb719c83aca200934f
vsingh7 <vsingh7@ncsu.edu>
2019-05-05 23:29:09 -0400

    added comments to CryptoUtil

--- src/main/java/gossip/CryptoUtil.java
+++ src/main/java/gossip/CryptoUtil.java
@@ -32,7 +32,24 @@ import java.nio.file.*;
 import java.security.*;
 import java.security.spec.*;
 
+/**
+ * CryptoUtil exposes APIs to easily sign 
+ * and verify a Network message.
+ *
+ * @author  Vaibhav Singh
+ * @version 1.0
+ * @since   2019-02-26
+ */
 public class CryptoUtil {
+
+    /**
+     * getHash returns the SHA-256 Hash of an input message.
+     *
+     * @param message Message to be hashed
+     * @return Hashed String
+     * @exception NoSuchAlgorithmException never thrown
+     * @see NoSuchAlgorithmException
+     */
      private static String getHash(String message) throws NoSuchAlgorithmException {
        MessageDigest md = MessageDigest.getInstance("SHA-256");
  
@@ -42,13 +59,33 @@ public class CryptoUtil {
        return String.format("%064x", new BigInteger(1, digest));
      }   
 
-      public static String getSignature(String message, PrivateKey key) throws NoSuchAlgorithmException, UnsupportedEncodingException, SignatureException, InvalidKeyException {
+     /** 
+      * getSignature returns the signed message.
+      *
+      * @param message Message to be signed.
+      * @param key PrivateKey object used to create the signature.
+      * @return Base64 encoded message signature
+      * @exception NoSuchAlgorithmException, UnsupportedEncodingException, SignatureException, InvalidKeyException
+      * @see NoSuchAlgorithmException, UnsupportedEncodingException, SignatureException, InvalidKeyException
+      */
+     public static String getSignature(String message, PrivateKey key) throws NoSuchAlgorithmException, UnsupportedEncodingException, SignatureException, InvalidKeyException {
         Signature sig = Signature.getInstance("SHA1WithRSA");
         sig.initSign(key);
         sig.update(message.getBytes("UTF-8"));
         byte[] signatureBytes = sig.sign();
         return Base64.getEncoder().encodeToString(signatureBytes);
       }  
+
+      /** 
+       * verifySignature verifies the signature on the message.
+       *
+       * @param message Message to be verified.
+       * @param sign signature of the Message
+       * @param key PublicKey object used to verify the signature.
+       * @return boolean whether the signature was correct
+       * @exception NoSuchAlgorithmException, UnsupportedEncodingException, SignatureException, InvalidKeyException
+       * @see NoSuchAlgorithmException, UnsupportedEncodingException, SignatureException, InvalidKeyException
+       */
       public static boolean verifySignature(String message, String sign, PublicKey key) throws NoSuchAlgorithmException, UnsupportedEncodingException, SignatureException, InvalidKeyException {
         Signature sig = Signature.getInstance("SHA1WithRSA");
         sig.initVerify(key);
@@ -56,6 +93,13 @@ public class CryptoUtil {
         return sig.verify(Base64.getDecoder().decode(sign));
       }  
 
+      /** 
+       * getPrivate gets the Private Key from a specified location.
+       *
+       * @param filename location of the file 
+       * @return private key
+       * @exception Exception
+       */
   public static PrivateKey getPrivate(String filename) throws Exception {
 
     byte[] keyBytes = Files.readAllBytes(Paths.get(filename));
@@ -66,6 +110,13 @@ public class CryptoUtil {
     return kf.generatePrivate(spec);
   }
 
+/** 
+ * getPublic gets the public key.
+ *
+ * @param filename name of the Public Key file.
+ * @return JCE PublicKey object representing the public key
+ * @exception Exception
+ */  
  public static PublicKey getPublic(String filename)
     throws Exception {
 
